a6471f4e4b79e056798b708534e644c6cc283148
Klemen Kenda <klemen.kenda@ijs.si>
2014-11-22 22:58:17 +0100

    Create epexsample.js

---
+++ epexsample.js
@@ -0,0 +1,34 @@
+    [{
+        "node": {
+            "id": "2",
+            "name": "spot-fr", "subjectid": "spot-fr", "lat": 46.19504, "lng": 2.10937,
+            "measurements": [{
+                "sensorid": "4", "value": 2475, "timestamp": "2005-04-22T00:00:00.000",
+                "type": {
+                    "id": "1", "name": "spot-fr-energy-price", "phenomenon": "total-energy",
+                    "UoM": "MWh"
+                }
+            },
+            {
+                "sensorid": "1", "value": 33.171, "timestamp": "2005-04-22T00:00:00.000",
+                "type": {
+                    "id": "2", "name": "spot-fr-energy-price", "phenomenon": "energy-pricing",
+                    "UoM": "EUR/MWh"
+                }
+            },
+            {
+                "sensorid": "1", "value": 32.054, "timestamp": "2005-04-22T01:00:00.000",
+                "type": {
+                    "id": "2", "name": "spot-fr-energy-price", "phenomenon": "energy-pricing",
+                    "UoM": "EUR/MWh"
+                }
+            },
+            {
+                "sensorid": "4", "value": 2711, "timestamp": "2005-04-22T01:00:00.000",
+                "type": {
+                    "id": "1", "name": "spot-fr-energy-price", "phenomenon": "total-energy",
+                    "UoM": "MWh"
+                }
+            }]
+        }
+    }];
